cf9aa6a65f051fa2c7af2fa8a7354d3babc924f6
MotionSwing <jon.white2@gmail.com>
2018-03-04 21:00:51 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GifTastic
+
+## Purpose
+
+A dynamic web page that displays 10 random gifs of your choosing based on which button you click. You can add new buttons by entering a topic to the right of the page. 
+
+## Author
+
+This project is maintained and written by 
+(Jon White) https://motionswing.github.io/
---
+++ assets/javascript/app.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+
+	const app = {
+		topics: ["dog","cat","rabbit","hamster","skunk","goldfish","bird","ferret",
+			"turtle","sugar glider","chinchilla","hedgehog","hermit crab","gerbil","pygmy goat",
+			"chicken","capybara","teacup pig","serval","salamander","frog"
+		],
+		gifIsPlaying: false,
+		initialize: function() {
+			$('.buttons').empty();
+			for (var i = 0; i < app.topics.length; i++) {
+				app.addAnimal(app.topics[i]);
+			}
+		},
+		addAnimal: function(newAnimal) {
+			const newButton = $("<button>").text(newAnimal);
+			const newDiv = $("<div>").append(newButton);
+			$('.buttons').append(newDiv);
+
+			// Remove listener from existing buttons and setup new listener on all buttons
+			$('.buttons button').off().on('click', function(event) {
+				$("#animals").empty();
+				$('.buttons button').removeClass('active');
+				$(this).addClass('active');
+				app.getGifsOf($(this).text());
+			});
+		},
+		getGifsOf: function(item){
+			// Setup Query String
+			queryURL = "http://api.giphy.com/v1/gifs/search?q="+ item +"&api_key=GNMNbc2t00Lgf7QWChNs9lquU7xNpDdp&limit=10"
+			$.ajax({
+				url: queryURL,
+				method: "GET"
+			}).then(function(response) {
+				console.log(response);
+				for (var i = 0; i < response.data.length; i++) {
+					// Setup still image
+					const img = $("<img>").attr({
+						"src": response.data[i].images.fixed_width_still.url,
+						"alt": response.data[i].slug
+					});
+
+					// Play/Pause the gif when the image is clicked
+					img.data({
+						"play-url": response.data[i].images.fixed_width.url,
+						"pause-url": response.data[i].images.fixed_width_still.url,
+						"status": "paused"
+					});
+					img.on('click', function() {
+						if($(this).data('status') === "paused") {
+							$(this).data('status','playing');
+							$(this).attr('src', $(this).data('play-url'));
+
+						}else {
+							$(this).data('status','paused');
+							$(this).attr('src', $(this).data('pause-url'));
+						}
+					});
+
+					// Create figure caption
+					const figCaption = $("<figcaption>").text("Rating: " + response.data[i].rating);
+					const fig = $("<figure>").append(figCaption, img);
+					$("#animals").append(fig);
+
+				}
+			});	
+		}
+	};
+
+	app.initialize();
+
+	// Event Handler for adding a new Animal
+	$('#addAnimal').on('click', function(event) {
+		event.preventDefault();
+		const selectedAnimal = $('#animal-input').val();
+		if (selectedAnimal){
+			app.addAnimal(selectedAnimal);
+		}
+	});
+
+});
+
